fe168045284150efc4a384f24746ad48271dc757
baiyi liu <1126865357@qq.com>
2024-03-05 15:44:51 +0800

    total 7

--- all2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python examples/run.py -c examples/STEMLP/STEMLP_PEMS08k1.py --gpus '0'
-python examples/run.py -c examples/STEMLP/STEMLP_PEMS08k2.py --gpus '0'
-python examples/run.py -c examples/STEMLP/STEMLP_PEMS08k3.py --gpus '0'
-python examples/run.py -c examples/STEMLP/STEMLP_PEMS08k4.py --gpus '0'
-python examples/run.py -c examples/STEMLP/STEMLP_PEMS08k5.py --gpus '0'
--- all3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python examples/run.py -c examples/STEMLP/STEMLP_PEMS04.py --gpus '0'
-python examples/run.py -c examples/STEMLP/STEMLP_PEMS07.py --gpus '0'
-python examples/run.py -c examples/STEMLP/STEMLP_PEMS08.py --gpus '0'
-python examples/run.py -c examples/STEMLP/STEMLP_TFA.py --gpus '0'
